8bcdae94ed19041b22f2e74553154f4f00d0cde6
Santiago Zapata <santiagozapata@Santiagos-MacBook-Pro.local>
2024-01-23 16:14:33 -0500

    updateS

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
 import React from "react";
 import { Header } from "./components/Header";
+import { MagnifyingGlassCircleIcon } from '@heroicons/react/24/outline';
+
 import { Card } from "./components/Card";
 
 
 export const App = () => {
   return (
-    <div className="bg-gradient-to-t from-blue-900 to-white w-full h-[100vh] -z-20 absolute inset-0">
-      <Header></Header>
+    <>
+      <form action="" className='flex justify-evenly absolute z-30 flex-col w-full '>
+        <h1 className='text-black font-semibold text-4xl'>Novedades del clima</h1>
+        <div className="flex">
+          <input type="text" placeholder="Buscar..." id="search-input" className='w-[20%] rounded-lg p-2 ' />
+          <button className='w-9 h-9 flex justify-center' type='submit'>
+            <MagnifyingGlassCircleIcon className='text-black' />
+          </button>
+        </div>
+      </form>
+      <div className="relative bg-blue-300 h-[100vh] w-full z-20">
 
-    </div>
-  )
-}
+
+        <div className="bg-gradient-to-t from-blue-300 via-transparent to-white/0 w-full relative h-[50vh] ">
+          <div className=" z-30">
+
+          </div>
+
+          <div className='text-center absolute inset-0 w-full'>
+            <Header />
+
+
+          </div>
+          {/* Agrega cualquier otro contenido que desees encima del fondo degradado */}
+        </div>
+      </div>
+    </>
+
+  );
+};
+
+
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,10 @@
-import { MagnifyingGlassCircleIcon } from '@heroicons/react/24/outline';
 import nubes from '../components/pexels_videos_2782-(1080p).mp4';
 
 export const Header = () => {
   return (
-    <section className='relative w-full h-[40vh] flex justify-center items-center'>
-      <div className='relative z-10 text-center'>
-        <h1 className='text-black font-semibold text-4xl'>Novedades del clima</h1>
-        <form action="" className='flex justify-evenly mt-8'>
-          <input type="text" placeholder="Buscar..." id="search-input" className='w-[70%] rounded-lg p-2' />
-          <button className='w-9 h-9 flex justify-center' type='submit'>
-            <MagnifyingGlassCircleIcon className='text-black' />
-          </button>
-        </form>
-      </div>
-      <video src={nubes} className='absolute inset-0 w-full h-full object-cover' autoPlay loop muted></video>
+    <section className='relative w-full h-[50vh] -z-10 flex justify-center items-center '>
+      
+      <video src={nubes} className='absolute inset-0 w-full h-full object-cover bg-gradient-to-t from-blue-300 via-transparent to-white/0' autoPlay loop muted></video>
     </section>
   );
 };
