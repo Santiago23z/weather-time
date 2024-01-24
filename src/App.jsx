503e3a98b9fa4b2701b524a9c2e0400eacfabfff
JuanCastao11 <juanmanuelcastano11b@gmail.com>
2024-01-23 19:11:44 -0500

    Mis hijos favoritos

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import { Header } from "./components/Header";
 import { MagnifyingGlassCircleIcon } from '@heroicons/react/24/outline';
-
+import { MagnifyingGlassIcon } from '@heroicons/react/16/solid'
+import './components/App.css'
 import { Card } from "./components/Card";
 
 
 export const App = () => {
   return (
     <>
-      <form action="" className='flex justify-evenly absolute z-30 flex-col w-full '>
-        <h1 className='text-black font-semibold text-4xl'>Novedades del clima</h1>
-        <div className="flex">
-          <input type="text" placeholder="Buscar..." id="search-input" className='w-[20%] rounded-lg p-2 ' />
-          <button className='w-9 h-9 flex justify-center' type='submit'>
-            <MagnifyingGlassCircleIcon className='text-black' />
+      <form action="" className='flex  absolute z-30 flex-col w-full h-[40vh] justify-center'>
+        <h1 className='text-black font-semibold flex justify-center text-4xl m-10  '>Novedades del clima</h1>
+        <div className="flex justify-center">
+          <input type="text" placeholder="Buscar..." id="search-input" className='w-[20%]  outline-none focus:outline-none border border-gray-300 px-4 py-2  rounded-lg p-2 ' />
+          <button className='w-9 h-9 flex justify-center ml-6 ' type='submit'>
+            <MagnifyingGlassIcon className='text-black' />
           </button>
         </div>
       </form>
@@ -27,11 +28,9 @@ export const App = () => {
 
           <div className='text-center absolute inset-0 w-full'>
             <Header />
-
-
           </div>
-          {/* Agrega cualquier otro contenido que desees encima del fondo degradado */}
         </div>
+        <Card/>
       </div>
     </>
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import Soleado from "../assets/img/img1.png"
 
 export const Card = () => {
   return (
-    <div>Card</div>
+    <aside className=' max-w-[350px] p-6  bg-white rounded-2xl  '>
+      <div className=" flex justify-between">
+        <h1 className='font-extrabold text-[1.8rem]'>Milan</h1>
+        <p className='font-semibold'>Soleado</p>
+      </div>
+      <h3 className='font-semibold'>10.3xm/s SW</h3>
+      <p className='font-semibold'>Feels like 13°C. Overcast clouds.
+        Strong breeze
+      </p>
+      <h3 className='font-semibold'>Humidity:83%</h3>
+      <div className=''>
+      <img src={Soleado} alt="" className=' ' />
+      </div>
+      <div className='flex flex-col items-end '>
+        <h1 className='order-1 font-extrabold text-[1.8rem]'>14°C</h1>
+        <p className='order-2 font-semibold'>18 march 2024, 6:02 p.m</p>
+      </div>
+
+    </aside>
   )
 }
