4802b0fadd49b99b17c9b972a7507049906acd35
JuanCastao11 <juanmanuelcastano11b@gmail.com>
2024-01-23 20:24:34 -0500

    Cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,20 +3,52 @@ import '../components/Card.css'
 
 export const Card = () => {
   return (
-    <aside className='max-w-[23rem] p-6 bg-white rounded-2xl relative h-[370px]'>
-      <div className="flex justify-between items-center">
-        <h1 className='font-extrabold text-[1.8rem]'>Milan</h1>
-        <p className='font-semibold'>Soleado</p>
-      </div>
-      <div>
-        <h3 className='font-semibold'>10.3xm/s SW</h3>
-        <p className='font-semibold'>Feels like 13°C. Overcast clouds. Strong breeze</p>
-        <h3 className='font-semibold'>Humidity: 83%</h3>
-      </div>
-      <div className='flex flex-col items-end'>
-        <h1 className='order-1 font-extrabold text-[1.8rem]'>14°C</h1>
-        <p className='order-2 font-semibold'>18 march 2024, 6:02 p.m</p>
-      </div>
-    </aside>
+<div className=' flex gap-32 justify-center'>
+  <aside className='max-w-[25rem] p-6 bg-white rounded-2xl relative h-[370px]'>
+    <div className="flex justify-between items-center">
+      <h1 className='font-extrabold text-[1.8rem]'>Milan</h1>
+      <p className='font-semibold'>Soleado</p>
+    </div>
+    <div>
+      <h3 className='font-semibold'>10.3xm/s SW</h3>
+      <p className='font-semibold'>Feels like 13°C. Overcast clouds. Strong breeze</p>
+      <h3 className='font-semibold'>Humidity: 83%</h3>
+    </div>
+    <div className='absolute inset-x-6 bottom-6 flex flex-col items-end'>
+      <h1 className='font-extrabold text-[1.8rem]'>30°C</h1>
+      <p className='font-semibold'>18 march 2024, 6:02 p.m</p>
+    </div>
+  </aside>
+  <aside className='max-w-[25rem] p-6 bg-white rounded-2xl relative h-[370px]'>
+    <div className="flex justify-between items-center">
+      <h1 className='font-extrabold text-[1.8rem]'>Paris</h1>
+      <p className='font-semibold'>Soleado</p>
+    </div>
+    <div>
+      <h3 className='font-semibold'>10.3xm/s SW</h3>
+      <p className='font-semibold'>Feels like 13°C. Overcast clouds. Strong breeze</p>
+      <h3 className='font-semibold'>Humidity: 83%</h3>
+    </div>
+    <div className='absolute inset-x-6 bottom-6 flex flex-col items-end'>
+      <h1 className='font-extrabold text-[1.8rem]'>28°C</h1>
+      <p className='font-semibold'>18 march 2024, 6:02 p.m</p>
+    </div>
+  </aside>
+  <aside className='max-w-[25rem] p-6 bg-white rounded-2xl relative h-[370px]'>
+    <div className="flex justify-between items-center">
+      <h1 className='font-extrabold text-[1.8rem]'>Medellin</h1>
+      <p className='font-semibold'>Soleado</p>
+    </div>
+    <div>
+      <h3 className='font-semibold'>10.3xm/s SW</h3>
+      <p className='font-semibold'>Feels like 13°C. Overcast clouds. Strong breeze</p>
+      <h3 className='font-semibold'>Humidity: 83%</h3>
+    </div>
+    <div className='absolute inset-x-6 bottom-6 flex flex-col items-end'>
+      <h1 className='font-extrabold text-[1.8rem]'>25°C</h1>
+      <p className='font-semibold'>18 march 2024, 6:02 p.m</p>
+    </div>
+  </aside>
+</div>
   );
 };
