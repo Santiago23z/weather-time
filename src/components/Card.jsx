b00578de501c9128cb80738a3de1749dac93276d
Santiago Zapata <santiagozapata@Santiagos-MacBook-Pro.local>
2024-01-23 19:53:59 -0500

    updates

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,22 @@
-import React from 'react'
-import Soleado from "../assets/img/img1.png"
+import React from 'react';
+import '../components/Card.css'
 
 export const Card = () => {
   return (
-    <aside className=' max-w-[350px] p-6  bg-white rounded-2xl  '>
-      <div className=" flex justify-between">
+    <aside className='max-w-[23rem] p-6 bg-white rounded-2xl relative h-[370px]'>
+      <div className="flex justify-between items-center">
         <h1 className='font-extrabold text-[1.8rem]'>Milan</h1>
         <p className='font-semibold'>Soleado</p>
       </div>
-      <h3 className='font-semibold'>10.3xm/s SW</h3>
-      <p className='font-semibold'>Feels like 13°C. Overcast clouds.
-        Strong breeze
-      </p>
-      <h3 className='font-semibold'>Humidity:83%</h3>
-      <div className=''>
-      <img src={Soleado} alt="" className=' ' />
+      <div>
+        <h3 className='font-semibold'>10.3xm/s SW</h3>
+        <p className='font-semibold'>Feels like 13°C. Overcast clouds. Strong breeze</p>
+        <h3 className='font-semibold'>Humidity: 83%</h3>
       </div>
-      <div className='flex flex-col items-end '>
+      <div className='flex flex-col items-end'>
         <h1 className='order-1 font-extrabold text-[1.8rem]'>14°C</h1>
         <p className='order-2 font-semibold'>18 march 2024, 6:02 p.m</p>
       </div>
-
     </aside>
-  )
-}
+  );
+};
